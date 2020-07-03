e7f5105ffaa916faa592b859bde66f5aee4302e3
GitShanks14 <59978777+GitShanks14@users.noreply.github.com>
2020-07-03 10:03:37 +0530

    Add files via upload

--- BioInformatics/Motif.py
+++ BioInformatics/Motif.py
@@ -15,6 +15,23 @@ def ReverseComplement ( Text ) :
         s += m [ i ]
     return s
 
+def PatternToNumber ( text ) :
+    s = 0
+    m = { 'A' : 0 , 'C' : 1 , 'G' : 2 , 'T' : 3 } 
+    for i in text :
+        s *= 4
+        s += m [ i ]
+    return s
+
+def NumberToPattern(index, k):
+    m = { 0 : 'A' , 1 : 'C' , 2 : 'G' , 3 : 'T' }
+    s = ''
+    while k :
+        k -= 1
+        s += m [ index % 4 ]
+        index //= 4
+    return s [ -1 : : -1 ]
+
 # Input:  Two strings p and q
 # Output: Hamming Distance between p and q.
 def HammingDistance(p, q):
@@ -126,6 +143,14 @@ def Score ( count ) :
         score += t - count [ c [ i ] ][ i ]
     return score
 
+# ScoreV2 is WORSE than V1. timed to be 20% SLOWER than Score. AVOID. 
+def ScoreV2 ( Motifs ) :
+    c = Consensus ( Count ( Motifs ) )
+    score = 0
+    for i in range ( len ( Motifs ) ) :
+        score += HammingDistance( Motifs [ i ] , c )
+    return score
+
 # Input:  A list of kmers Motifs
 # Output: the profile matrix of Motifs, as a dictionary of lists.
 def Profile ( Motifs ) :
@@ -152,6 +177,76 @@ def Entropy ( profile ) :
     p = - profile * np . log2 ( profile + 0.00000000001 )
     p = np . sum ( p )
     return p
+
+# Input  : A pattern and a list of DNA strings of same length 
+# Output : The best motif set built around the pattern is selected and returned
+#           The score and Motif set are returned as a tuple. 
+# O ( n.t.k )
+def BestMotifs ( Pattern , Dna ) :
+    Motifs = [ ]
+    score = 0
+    k = len ( Pattern )
+    for i in range ( len ( Dna ) ) :                    # t steps
+        d = 100000
+        p = None
+        for j in range ( len ( Dna [ 0 ] ) - k + 1 ) :  # n - k ~ n steps
+            d2 = HammingDistance ( Dna [ i ][ j : j + k ] , Pattern ) # k steps
+            if ( d2 < d ) :
+                d = d2
+                p = Dna [ i ][ j : j + k ]
+        Motifs . append ( p )
+        score += d 
+    return score , Motifs
+            
+# BRUTE FORCE MOTIF SEARCH: Traverses ALL possible kmers to find the one for 
+# which the lowest scoring motif set exists. DOUBLE MINIMISATION PROBLEM
+# Input : A list of strings of Dna of equal length, length k of pattern to be found
+# INEFFICIENT. 4^k can be narrowed down dramatically. 
+# Method may not be improved upon, due to existence of more promising alternative.
+def BFMotifSearch ( Dna , k ) :
+    Motifs = [ ]
+    p = [ ]
+    d = 10000000
+    for i in range ( 4 ** k ) :
+        p2 = NumberToPattern( i , k )
+        d2 , m2 = BestMotifs ( p2 , Dna )
+        if ( d2 < d ) :
+            p . clear ( )
+            Motifs . clear ( )
+            p . append ( p2 )
+            Motifs . append ( m2 )
+            d = d2
+        elif ( d2 == d ) :
+            p . append ( p2 )
+            Motifs . append ( m2 )
+    return d , Motifs , p
+            
+###############################################################################
+#                            TESTING GROUNDS                                  #
+###############################################################################
+
+import time
+t0 = time . time ( )
+
+def timer ( ):
+    global t0
+    print ( "Execution of block took {} s" . format ( time . time ( ) - t0 ) )
+    t0 = time . time ( )
     
+
+k = 6
+
+with open ( '/Users/sashank/Desktop/Data/d.txt' , mode = 'r') as f:
+    genome = f . readline ( 10000000000000000 )
+    Dna = genome . split ( )
+    print ( "Data read from file." )
+    score , motifs , p = BFMotifSearch ( Dna , k )
+    print ( score ) 
+    for i in p :
+        print ( i , end = ' ' )
+    print ( '' )
+    timer ( )
+    
+#### DOCUMENT THE CODE!!!
 
     
