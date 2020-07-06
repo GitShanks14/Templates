63e620193d3f55733a0f0d2762067859c601894c
GitShanks14 <59978777+GitShanks14@users.noreply.github.com>
2020-07-06 20:22:58 +0530

    Update p1.py

--- FileHanding/p1.py
+++ FileHanding/p1.py
@@ -3,7 +3,9 @@
 with open ( '<InsertFileName' , mode = 'r' ) as f :
     f . readline ( 10000000000 ) # Reads one long line
 
-with open ( '/Users/sashank/Desktop/Data/out.txt' , mode = 'w' ) as f :
-    for i in l :                       #Writes list entries line by line
-        f . write ( str ( i ) + '\n' )
+with open ( '/Users/sashank/Desktop/Data/d.txt' , mode = 'r') as f:
+    s = f . read ( ) . split ( ) # Read list with one entry per line
 
+with open ( '/Users/sashank/Desktop/Data/out.txt' , mode = 'w' ) as f : #Store list line by line
+    for i in l :        
+        f . write ( str ( i ) + '\n' )
