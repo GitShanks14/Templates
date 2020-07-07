80b64d101d418193def4164a75f7635b94212b29
GitShanks14 <59978777+GitShanks14@users.noreply.github.com>
2020-07-07 16:18:58 +0530

    Update p1.py

--- FileHanding/p1.py
+++ FileHanding/p1.py
@@ -9,3 +9,23 @@ with open ( '/Users/sashank/Desktop/Data/d.txt' , mode = 'r') as f:
 with open ( '/Users/sashank/Desktop/Data/out.txt' , mode = 'w' ) as f : #Store list line by line
     for i in l :        
         f . write ( str ( i ) + '\n' )
+
+        
+###################################################################################################################
+# RANDOM ONE TIME USE STUFF FOR A COURSE THAT I DON'T WANT TO DELETE BECAUSE I MIGHT HAVE TO REUSE SOON           #
+###################################################################################################################
+# WRITE GRAPH IN FILE
+for key , val in l :        
+        k = len ( val )
+        if ( k == 0 ) :
+            continue 
+        i = 0
+        f . write ( str ( key ) + ' -> ' )
+        while ( i < k ) :
+            f . write ( val [ i ] )
+            if ( i != k - 1 ) :
+                f . write ( ',' )
+            i += 1
+        f . write ( '\n' )
+ 
+f . write ( '->' . join ( [ str ( val ) for val in Path ] ) ) # WRITE PATH IN FILE
