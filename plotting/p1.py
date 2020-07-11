29604bc788fa22bba96ec76571e89fe28cd2995b
GitShanks14 <59978777+GitShanks14@users.noreply.github.com>
2020-07-12 03:04:49 +0530

    Update p1.py

--- plotting/p1.py
+++ plotting/p1.py
@@ -30,6 +30,8 @@ ax2 . set_ylabel ( "Sinh" , color = curve2 . get_color ( ) )
 # Graph sheet settings
 ax1 . tick_params ( axis = 'x' , color = 'c' )
 ax1 . set_xticks ( x_ticks )
+ax1 . set_yticks ( y_ticks1 )
+ax2 . set_yticks ( y_ticks2 ) 
 
 '''
 # 1. If the desired grid is wrto y axis 1
@@ -44,6 +46,48 @@ ax1 . yaxis . grid ( False )# To hide the grid wrto y axis of plot 1 as goal is
 
 plt . title ( "Real vs Imaginary inputs")
 
+################################################ Different X axis scale but same Y axis scale ###############################
+
+# Initialise data
+x1 = np . sin ( y )
+x2 = np . sinh ( y ) 
+y = np . linspace ( 0 , 7 , 15 )
+
+# Set spacing
+y_ticks = np . linspace ( 0 , 7 , 15 )
+x_ticks1 = np . linspace ( -1 , 1 , 11 )
+x_ticks2 = np . linspace ( 0 , 300 , 7 ) 
+
+# Create subplots, edit separately and "superposition" the plots
+figure , ax1 = plt . subplots ( )
+ax2 = ax1 . twiny ( )
+curve1, = ax1 . plot ( x1 , y , color = 'c' , label = 'Sine Inverse' )
+curve2, = ax2 . plot ( x2 , y , color = 'm' , label = 'Sinh Inverse' )
+curves = [ curve1 , curve2 ]
+
+# Label the graph
+ax2 . legend ( curves , [ curve . get_label ( )  for curve in curves ] )
+ax1 . set_ylabel ( "Angle" , color = curve2 . get_color ( ) ) 
+ax1 . set_xlabel ( "ArcSine" , color = curve1 . get_color ( ) )
+ax2 . set_xlabel ( "ArcSinh" , color = curve2 . get_color ( ) )
+
+# Graph sheet settings
+ax1 . tick_params ( axis = 'x' , color = curve1 . get_color ( ) )
+ax2 . tick_params ( axis = 'x' , color = curve2 . get_color ( ) )
+ax1 . set_xticks ( x_ticks1 )
+ax1 . set_xticks ( x_ticks2 )
+ax1 . set_yticks ( y_ticks  )
+'''
+# 1. If the desired grid is wrto y axis 1
+ax1 . grid ( color = 'g' )
+
+# 2. If the desired grid is wrto y axis 2
+ax1 . grid ( color = 'g' )  # To print grid from x axis too
+ax2 . grid ( color = 'g' )  # To print grid wrto y axis of plot 2
+ax1 . xaxis . grid ( False )# To hide the grid wrto y axis of plot 1 as goal is 
+                            # to print grid wrto only y axis of plot 2
+
+plt . title ( "Real vs Imaginary inputs")
 
 
 
