f503c9745124855744a6901c4cb746d6f41a5d24
GitShanks14 <59978777+GitShanks14@users.noreply.github.com>
2020-07-19 04:33:43 +0530

    Update antibio.py

--- BioInformatics/antibio.py
+++ BioInformatics/antibio.py
@@ -168,3 +168,97 @@ def CycloPeptideSequencing ( Spectrum ) :
         print ( len ( Candidates ) ) 
     return FinalPeptides 
 
+########################################################################################################################################################
+# The following code represents peptides as strings of characters, and assumes the basic Peptide Set                                                   #
+########################################################################################################################################################
+
+def Mass ( Peptide ) :
+    m = 0
+    for i in Peptide :
+        m += mass [ i ]
+    return m
+        
+def CyclicScore ( Peptide , Spectrum ) :
+    score = 0
+    s = CyclicSpectrum ( Peptide )
+    for i in s : 
+        if i in Spectrum :
+            score += 1
+            Spectrum . remove ( i )
+    return score
+
+def LinearScore ( Peptide , Spectrum ) :
+    score = 0
+    s = LinearSpectrum ( Peptide )
+    for i in s : 
+        if i in Spectrum :
+            score += 1
+            Spectrum . remove ( i )
+    return score
+
+def LeaderboardCycloPeptideSequencing ( Spectrum , N ):
+    Leaderboard = [ '' ]        # '' -> empty list
+    LeaderPeptides = [ ]        # -> list of lists
+    Lscore = 0
+    pmass = Spectrum [ -1 ]     
+    while Leaderboard :
+        print ( len ( Leaderboard ) , '->' , end = ' ' )
+        NewCandidates = [ ]
+        # Grow
+        for i in Leaderboard :
+            for j in AminoAcids :       # AA -> list of ints = masses
+                NewCandidates . append ( str ( i ) + str ( j ) )    # simply append new mass
+        Leaderboard = NewCandidates . copy ( )
+        # Filter
+        for i in Leaderboard . copy ( ) :   # i is NOT a peptide. It's a peptide only by mass
+            m = Mass ( i )                  # Rewrite Mass
+            if m == pmass :
+                score = CyclicScore ( i , Spectrum . copy ( ) ) # Rewrite Scoring
+                if score > Lscore :
+                    Lscore = score 
+                    LeaderPeptides . clear ( )
+                    LeaderPeptides . append ( i )
+                elif score == Lscore :
+                    LeaderPeptides . append ( i )
+            elif m > pmass :
+                    Leaderboard . remove ( i )   
+        #Trim
+        Leaderscores = [ ]
+        for i in Leaderboard :
+            Leaderscores . append ( LinearScore ( i , Spectrum . copy ( ) ) )   # Rewrite Scoring
+        Leaderboard . sort ( key = lambda x : LinearScore ( x , Spectrum . copy () ) , reverse = True )
+        Leaderscores . sort ( reverse = True )
+        if ( N < len ( Leaderboard ) ) :
+            score = Leaderscores [ N - 1 ]
+        for i in range ( N , len ( Leaderboard ) ) :
+            if Leaderscores [ i ] < score :
+                Leaderboard = Leaderboard [ : i ]
+                break
+        print ( len ( Leaderboard ) )
+    return LeaderPeptides
+    
+def Trim ( Leaderboard , Spectrum , N ) : #Slower as it => function call. 
+    Leaderscores = [ ]
+    for i in Leaderboard :
+        Leaderscores . append ( LinearScore ( i , Spectrum . copy ( ) ) )
+    Leaderboard . sort ( key = lambda x : LinearScore ( x , Spectrum . copy () ) , reverse = True )
+    Leaderscores . sort ( reverse = True )
+    if ( N < len ( Leaderboard ) ) :
+        score = Leaderscores [ N - 1 ]
+    for i in range ( N , len ( Leaderboard ) ) :
+        if Leaderscores [ i ] < score :
+            Leaderboard = Leaderboard [ : i ]
+            break
+    return Leaderboard
+
+def Convolution ( Spectrum ) :
+    Convolution = [ ]
+    n = len ( Spectrum )
+    for i in range ( n ) :
+        for j in range ( i + 1 , n ) :
+            Convolution . append ( Spectrum [ j ] - Spectrum [ i ] )
+    return Convolution            
+
+################################################################################################################################################################
+# The following code is equivalent to the above code, but uses lists of masses and not strings of characters to represent peptides
+################################################################################################################################################################
